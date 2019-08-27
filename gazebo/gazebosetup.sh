cf506c5e0353656f04c349fcb4811d4eee5183a4
rachelruiliu <rachel.ruiliu@etpartners.com.au>
2019-08-27 16:48:19 +0800

    gazebo 7 with web mode roughly works

--- gazebo/gazebosetup.sh
+++ gazebo/gazebosetup.sh
@@ -1,7 +1,8 @@
 #http://gazebosim.org/tutorials?tut=install_ubuntu&cat=install
 #curl -sSL http://get.gazebosim.org | sh
-sudo apt install gazebo7 libgazebo7-dev
-sudo apt install libjansson-dev nodejs npm nodejs-legacy libboost-dev imagemagick libtinyxml-dev mercurial cmake build-essential
+echo y| sudo apt install gazebo7
+echo y| sudo apt install libgazebo7-dev
+echo y| sudo apt install libjansson-dev nodejs npm nodejs-legacy libboost-dev imagemagick libtinyxml-dev mercurial cmake build-essential
 
 sudo apt-get -qq install mercurial
 cd ~; hg clone https://bitbucket.org/osrf/gzweb
@@ -10,7 +11,7 @@ hg up gzweb_1.4.0
 source /usr/share/gazebo/setup.sh
 
 
-sudo apt-get -qq install npm
-sudo apt-get -qq install libjansson-dev
-sudo apt-get -qq install imagemagick
-npm run deploy --- -m
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+sudo npm run deploy --- -m
--- gazebo/readme.md
+++ gazebo/readme.md
@@ -11,7 +11,14 @@ git clone git@github.com:racheliurui/vagrant.git
 ```shell
 cd ~/github/vagrant/gazebo
 vagrant up
+```
+
+```shell
 vagrant ssh -- -vvv -X
+gzserver --verbose
+cd ~/gzweb
+npm start
+vagrant ssh -- -L 8080:localhost:8080
 ```
 
 # Enable GUI for ubuntu vagrant
